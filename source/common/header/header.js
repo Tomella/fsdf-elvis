40de7d01055990bfaa89e64bdcc33be4b23d5855
Larry Hill <tomella1@gmail.com>
2016-10-20 11:44:08 +1100

    Update header to match latest discussion

--- source/common/header/header.js
+++ source/common/header/header.js
@@ -1,59 +1,59 @@
 
-(function(angular) {
+(function (angular) {
 
-'use strict';
+	'use strict';
 
-angular.module('common.header', [])
+	angular.module('common.header', [])
 
-.controller('headerController', [ '$scope', '$q', '$timeout', function ($scope, $q, $timeout) {
+		.controller('headerController', ['$scope', '$q', '$timeout', function ($scope, $q, $timeout) {
 
-    var modifyConfigSource = function (headerConfig) {
-        return headerConfig;
-    };
+			var modifyConfigSource = function (headerConfig) {
+				return headerConfig;
+			};
 
-    $scope.$on('headerUpdated', function (event, args) {
-        $scope.headerConfig = modifyConfigSource(args);
-    });
-}])
-
-.directive('icsmHeader', [function() {
-	var defaults = {
-		heading:"ICSM",
-		headingtitle:"ICSM",
-		helpurl:"help.html",
-		helptitle:"Get help about ICSM",
-		helpalttext:"Get help about ICSM",
-		skiptocontenttitle:"Skip to content",
-		skiptocontent:"Skip to content",
-		quicklinksurl:"/search/api/quickLinks/json?lang=en-US"
-	};
-	return {
-		transclude:true,
-		restrict:'EA',
-		templateUrl:"common/header/header.html",
-		scope : {
-			breadcrumbs: "=",
-			heading: "=",
-			headingtitle:"=",
-			helpurl:"=",
-			helptitle:"=",
-			helpalttext:"=",
-			skiptocontenttitle:"=",
-			skiptocontent:"=",
-			quicklinksurl:"="
-		},
-		link:function(scope, element, attrs) {
-			var data = angular.copy(defaults);
-			angular.forEach(defaults, function(value, key) {
-				if(!(key in scope)) {
-					scope[key] = value;
-				}
+			$scope.$on('headerUpdated', function (event, args) {
+				$scope.headerConfig = modifyConfigSource(args);
 			});
-		}
-	};
-}])
+		}])
 
-.factory('headerService', ['$http', function() {
-}]);
+		.directive('icsmHeader', [function () {
+			var defaults = {
+				heading: "ICSM",
+				headingtitle: "ICSM",
+				helpurl: "help.html",
+				helptitle: "Get help about ICSM",
+				helpalttext: "Get help about ICSM",
+				skiptocontenttitle: "Skip to content",
+				skiptocontent: "Skip to content",
+				quicklinksurl: "/search/api/quickLinks/json?lang=en-US"
+			};
+			return {
+				transclude: true,
+				restrict: 'EA',
+				templateUrl: "common/header/header.html",
+				scope: {
+					breadcrumbs: "=",
+					heading: "=",
+					headingtitle: "=",
+					helpurl: "=",
+					helptitle: "=",
+					helpalttext: "=",
+					skiptocontenttitle: "=",
+					skiptocontent: "=",
+					quicklinksurl: "="
+				},
+				link: function (scope, element, attrs) {
+					var data = angular.copy(defaults);
+					angular.forEach(defaults, function (value, key) {
+						if (!(key in scope)) {
+							scope[key] = value;
+						}
+					});
+				}
+			};
+		}])
+
+		.factory('headerService', ['$http', function () {
+		}]);
 
 })(angular);
