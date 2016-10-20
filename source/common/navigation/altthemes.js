c1a2a0aa862904972f825b94aa7abf4c5afa9b91
Larry Hill <tomella1@gmail.com>
2016-10-20 11:45:21 +1100

    Update themes so you can configure them on and off

--- source/common/navigation/altthemes.js
+++ source/common/navigation/altthemes.js
@@ -1,114 +1,125 @@
- (function(angular) {
+(function (angular) {
 
-'use strict';
+	'use strict';
 
-angular.module('common.altthemes', [])
+	angular.module('common.altthemes', [])
 
-	/**
- 	 *
- 	 * Override the original mars user.
- 	 *
-  	 */
-	.directive('altThemes', ['altthemesService', function(themesService) {
-		return {
-			restrict: 'AE',
-			templateUrl: 'common/navigation/altthemes.html',
-			link: function(scope) {
-				themesService.getThemes().then(function(themes) {
-					scope.themes = themes;
-				});
+		/**
+			*
+			* Override the original mars user.
+			*
+			  */
+		.directive('altThemes', ['altthemesService', function (themesService) {
+			return {
+				restrict: 'AE',
+				templateUrl: 'common/navigation/altthemes.html',
+				link: function (scope) {
+					themesService.getThemes().then(function (themes) {
+						scope.themes = themes;
+					});
 
-				themesService.getCurrentTheme().then(function(theme) {
-					scope.theme = theme;
-				});
+					themesService.getCurrentTheme().then(function (theme) {
+						scope.theme = theme;
+					});
 
-				scope.changeTheme = function(theme) {
-					scope.theme = theme;
-					themesService.setTheme(theme.key);
-				};
-			}
-		};
-   }])
+					scope.changeTheme = function (theme) {
+						scope.theme = theme;
+						themesService.setTheme(theme.key);
+					};
+				}
+			};
+		}])
 
-	.controller('altthemesCtrl', ['altthemesService', function(altthemesService) {
-		this.service = altthemesService;
-	}])
+		.controller('altthemesCtrl', ['altthemesService', function (altthemesService) {
+			this.service = altthemesService;
+		}])
 
-	.filter('altthemesFilter', function() {
-   	return function(features, theme) {
-			var response = [];
-			// Give 'em all if they haven't set a theme.
-			if(!theme) {
-				return features;
-			}
+		.filter('altthemesFilter', function () {
+			return function (features, theme) {
+				var response = [];
+				// Give 'em all if they haven't set a theme.
+				if (!theme) {
+					return features;
+				}
 
-			if(features) {
-				features.forEach(function(feature) {
-					if(feature.themes) {
-         			if( feature.themes.some(function(name) {
-							return name == theme.key;
-						})) {
-							response.push(feature);
+				if (features) {
+					features.forEach(function (feature) {
+						if (feature.themes) {
+							if (feature.themes.some(function (name) {
+								return name === theme.key;
+							})) {
+								response.push(feature);
+							}
 						}
-      			}
-      		});
-			}
-			return response;
-   	};
-	})
+					});
+				}
+				return response;
+			};
+		})
 
-	.factory('altthemesService', ['$q', '$http', 'storageService', function($q, $http, storageService) {
-		var THEME_PERSIST_KEY = 'icsm.current.theme';
-      var THEMES_LOCATION = 'icsm/resources/config/themes.json';
-		var DEFAULT_THEME = "All";
-		var waiting = [];
-		var self = this;
+		.factory('altthemesService', ['$q', '$http', 'storageService', function ($q, $http, storageService) {
+			var THEME_PERSIST_KEY = 'icsm.current.theme';
+			var THEMES_LOCATION = 'icsm/resources/config/themes.json';
+			var DEFAULT_THEME = "All";
+			var waiting = [];
+			var self = this;
 
-		this.themes = [];
-		this.theme = null;
+			this.themes = [];
+			this.theme = null;
 
-		storageService.getItem(THEME_PERSIST_KEY).then(function(value) {
-			if(!value) {
-				value = DEFAULT_THEME;
-			}
-			$http.get(THEMES_LOCATION, {cache: true}).then(function(response) {
-            var themes = response.data.themes;
+			storageService.getItem(THEME_PERSIST_KEY).then(function (value) {
+				if (!value) {
+					value = DEFAULT_THEME;
+				}
+				$http.get(THEMES_LOCATION, { cache: true }).then(function (response) {
+					var themes = response.data.themes;
 
-				self.themes = themes;
-				self.theme = themes[value];
-				// Decorate the key
-				angular.forEach(themes, function(theme, key) {
-					theme.key = key;
-				});
-				waiting.forEach(function(wait) {
+					self.themes = themes;
+					self.theme = themes[value];
+					// Decorate the key
+					angular.forEach(themes, function (theme, key) {
+						theme.key = key;
+					});
+					waiting.forEach(function (wait) {
 						wait.resolve(self.theme);
+					});
 				});
 			});
+
+
+			this.getCurrentTheme = function () {
+				if (this.theme) {
+					return $q.when(self.theme);
+				} else {
+					var waiter = $q.defer();
+					waiting.push(waiter);
+					return waiter.promise;
+				}
+			};
+
+			this.getThemes = function () {
+				return $http.get(THEMES_LOCATION, { cache: true }).then(function (response) {
+					return response.data.themes;
+				});
+			};
+
+			this.setTheme = function (key) {
+				this.theme = this.themes[key];
+				storageService.setItem(THEME_PERSIST_KEY, key);
+			};
+
+			return this;
+		}])
+
+		.filter('altthemesEnabled', function () {
+			return function (headers) {
+				if (headers) {
+					return headers.filter(function (value) {
+						return !!value.enabled;
+					});
+				}
+				return headers;
+			};
 		});
-
-
-		this.getCurrentTheme = function(){
-			if(this.theme) {
-				return $q.when(self.theme);
-			} else {
-				var waiter = $q.defer();
-				waiting.push(waiter);
-				return waiter.promise;
-			}
-		};
-
-		this.getThemes = function() {
-			return $http.get(THEMES_LOCATION, {cache: true}).then(function(response) {
-            return response.data.themes;
-         });
-		};
-
-		this.setTheme = function(key) {
-			this.theme = this.themes[key];
-			storageService.setItem(THEME_PERSIST_KEY, key);
-		};
-
-		return this;
-	}]);
 
 })(angular);
