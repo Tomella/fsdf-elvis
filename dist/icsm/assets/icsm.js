11e9add205a04480ba3845c93d888c5aa567fb45
Larry Hill <tomella1@gmail.com>
2019-05-08 10:59:58 +0930

    Make the elevation service request protocol agnostic

--- dist/icsm/assets/icsm.js
+++ dist/icsm/assets/icsm.js
@@ -17,239 +17,6 @@ specific language governing permissions and limitations
 under the License.
 */
 
-"use strict";
-
-{
-   var fileSize = function fileSize(size) {
-      var meg = 1000 * 1000;
-      var gig = meg * 1000;
-      var ter = gig * 1000;
-
-      if (!size) {
-         return "-";
-      }
-
-      if (("" + size).indexOf(" ") > -1) {
-         return size;
-      }
-
-      size = parseFloat(size);
-
-      if (size < 1000) {
-         return size + " bytes";
-      }
-      if (size < meg) {
-         return (size / 1000).toFixed(1) + " kB";
-      }
-      if (size < gig) {
-         return (size / meg).toFixed(1) + " MB";
-      }
-      if (size < ter) {
-         return (size / gig).toFixed(1) + " GB";
-      }
-      return (size / ter).toFixed(1) + " TB";
-   };
-
-   angular.module("icsm.bounds", ["icsm.message"]).directive('icsmBounds', ['$rootScope', 'icsmMessageService', 'boundsService', function ($rootScope, icsmMessageService, boundsService) {
-      return {
-         restrict: 'AE',
-         link: function link() {
-            boundsService.init().then(null, null, function notify(message) {
-               icsmMessageService.removeFlash();
-               switch (message.type) {
-                  case "error":
-                  case "warn":
-                  case "info":
-                     icsmMessageService[message.type](message.text);
-                     break;
-                  case "wait":
-                     icsmMessageService.wait(message.text);
-                     break;
-                  default:
-                     icsmMessageService.flash(message.text, message.duration ? message.duration : 8000, message.type === "wait");
-               }
-            });
-         }
-      };
-   }]).factory("boundsService", ['$http', '$q', '$rootScope', '$timeout', 'configService', 'flashService', 'messageService', function ($http, $q, $rootScope, $timeout, configService, flashService, messageService) {
-      var clipTimeout = void 0,
-          notify = void 0;
-      return {
-         init: function init() {
-            notify = $q.defer();
-            $rootScope.$on('icsm.clip.drawn', function (event, clip) {
-               send('Area drawn. Checking for data...');
-               _checkSize(clip).then(function (message) {
-                  if (message.code === "success") {
-                     getList(clip);
-                  } else {
-                     // $rootScope.$broadcast('icsm.clip.draw', { message: "oversize" });
-                  }
-               });
-            });
-            return notify.promise;
-         },
-
-         cancelDraw: function cancelDraw() {
-            drawService.cancelDrawRectangle();
-         },
-
-         checkSize: function checkSize(clip) {
-            return _checkSize(clip);
-         }
-      };
-
-      function send(message, type, duration) {
-         flashService.remove(notify);
-
-         if (message) {
-            if (type === "error") {
-               messageService.error(message);
-            } else {
-               notify = flashService.add(message, duration, true);
-            }
-         }
-         /*
-           if (notify) {
-            notify.notify({
-               text: message,
-               type: type,
-               duration: duration
-            });
-         }
-         */
-      }
-
-      function _checkSize(clip) {
-         var deferred = $q.defer();
-         var result = drawn(clip);
-         if (result && result.code) {
-            switch (result.code) {
-               case "oversize":
-                  $timeout(function () {
-                     send("", "clear");
-                     send("The selected area is too large to process. Please restrict to approximately " + "1.5 degrees square.", "error");
-                     deferred.resolve(result);
-                  });
-                  break;
-               case "undersize":
-                  $timeout(function () {
-                     send("", "clear");
-                     send("X Min and Y Min should be smaller than X Max and Y Max, respectively. " + "Please update the drawn area.", "error");
-                     deferred.resolve(result);
-                  });
-                  break;
-               default:
-                  return $q.when(result);
-            }
-         }
-         return deferred.promise;
-      }
-
-      function underSizeLimit(clip) {
-         var size = (clip.xMax - clip.xMin) * (clip.yMax - clip.yMin);
-         return size < 0.00000000001 || clip.xMax < clip.xMin;
-      }
-
-      function overSizeLimit(clip) {
-         // Shouldn't need abs but it doesn't hurt.
-         var size = Math.abs((clip.xMax - clip.xMin) * (clip.yMax - clip.yMin));
-         return size > 2.25;
-      }
-
-      function forceNumbers(clip) {
-         clip.xMax = clip.xMax === null ? null : +clip.xMax;
-         clip.xMin = clip.xMin === null ? null : +clip.xMin;
-         clip.yMax = clip.yMax === null ? null : +clip.yMax;
-         clip.yMin = clip.yMin === null ? null : +clip.yMin;
-      }
-
-      function drawn(clip) {
-         //geoprocessService.removeClip();
-         forceNumbers(clip);
-
-         if (overSizeLimit(clip)) {
-            return { code: "oversize" };
-         }
-
-         if (underSizeLimit(clip)) {
-            return { code: "undersize" };
-         }
-
-         if (clip.xMax === null) {
-            return { code: "incomplete" };
-         }
-
-         if (validClip(clip)) {
-            return { code: "success" };
-         }
-         return { code: "invalid" };
-      }
-
-      // The input validator takes care of order and min/max constraints. We just check valid existance.
-      function validClip(clip) {
-         return clip && angular.isNumber(clip.xMax) && angular.isNumber(clip.xMin) && angular.isNumber(clip.yMax) && angular.isNumber(clip.yMin) && !overSizeLimit(clip) && !underSizeLimit(clip);
-      }
-
-      function getList(clip) {
-         configService.getConfig("processing").then(function (conf) {
-            var url = conf.intersectsUrl;
-            if (url) {
-               // Order matches the $watch signature so be careful
-               var urlWithParms = url.replace("{maxx}", clip.xMax).replace("{minx}", clip.xMin).replace("{maxy}", clip.yMax).replace("{miny}", clip.yMin);
-
-               send("Checking there is data in your selected area...", "wait", 180000);
-               $http.get(urlWithParms).then(function (response) {
-                  if (response.data && response.data.available_data) {
-                     var hasData = false;
-                     send("", "clear");
-                     if (response.data.available_data) {
-                        response.data.available_data.forEach(function (group) {
-                           if (group.downloadables) {
-                              decorateDownloadables(group.downloadables);
-                              hasData = true;
-                           }
-                        });
-                     }
-                     if (!hasData) {
-                        send("There is no data held in your selected area. Please try another area.", null, 4000);
-                     }
-
-                     $rootScope.$broadcast('site.selection', response.data);
-                  }
-               }, function (err) {
-                  // If it falls over we don't want to crash.
-                  send("The service that provides the list of datasets is currently unavailable. " + "Please try again later.", "error");
-               });
-            }
-         });
-
-         function decorateDownloadables(downloadables) {
-            Object.keys(downloadables).forEach(function (groupname) {
-               var group = downloadables[groupname];
-               Object.keys(group).forEach(function (listName) {
-                  var items = group[listName];
-
-                  items.forEach(function (item) {
-                     return decorateItem(item);
-                  });
-               });
-            });
-         }
-
-         function decorateItem(item) {
-            item.fileSize = fileSize(item.file_size);
-            if (item.product) {
-               //  "bbox" : "113,-44,154,-10"
-               var arr = item.bbox.split(",").map(function (num) {
-                  return +num;
-               });
-               item.bbox = [Math.max(arr[0], clip.xMin), Math.max(arr[1], clip.yMin), Math.min(arr[2], clip.xMax), Math.min(arr[3], clip.yMax)].join(",");
-            }
-         }
-      }
-   }]);
-}
 'use strict';
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -501,6 +268,239 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
             });
          }
       };
+   }]);
+}
+"use strict";
+
+{
+   var fileSize = function fileSize(size) {
+      var meg = 1000 * 1000;
+      var gig = meg * 1000;
+      var ter = gig * 1000;
+
+      if (!size) {
+         return "-";
+      }
+
+      if (("" + size).indexOf(" ") > -1) {
+         return size;
+      }
+
+      size = parseFloat(size);
+
+      if (size < 1000) {
+         return size + " bytes";
+      }
+      if (size < meg) {
+         return (size / 1000).toFixed(1) + " kB";
+      }
+      if (size < gig) {
+         return (size / meg).toFixed(1) + " MB";
+      }
+      if (size < ter) {
+         return (size / gig).toFixed(1) + " GB";
+      }
+      return (size / ter).toFixed(1) + " TB";
+   };
+
+   angular.module("icsm.bounds", ["icsm.message"]).directive('icsmBounds', ['$rootScope', 'icsmMessageService', 'boundsService', function ($rootScope, icsmMessageService, boundsService) {
+      return {
+         restrict: 'AE',
+         link: function link() {
+            boundsService.init().then(null, null, function notify(message) {
+               icsmMessageService.removeFlash();
+               switch (message.type) {
+                  case "error":
+                  case "warn":
+                  case "info":
+                     icsmMessageService[message.type](message.text);
+                     break;
+                  case "wait":
+                     icsmMessageService.wait(message.text);
+                     break;
+                  default:
+                     icsmMessageService.flash(message.text, message.duration ? message.duration : 8000, message.type === "wait");
+               }
+            });
+         }
+      };
+   }]).factory("boundsService", ['$http', '$q', '$rootScope', '$timeout', 'configService', 'flashService', 'messageService', function ($http, $q, $rootScope, $timeout, configService, flashService, messageService) {
+      var clipTimeout = void 0,
+          notify = void 0;
+      return {
+         init: function init() {
+            notify = $q.defer();
+            $rootScope.$on('icsm.clip.drawn', function (event, clip) {
+               send('Area drawn. Checking for data...');
+               _checkSize(clip).then(function (message) {
+                  if (message.code === "success") {
+                     getList(clip);
+                  } else {
+                     // $rootScope.$broadcast('icsm.clip.draw', { message: "oversize" });
+                  }
+               });
+            });
+            return notify.promise;
+         },
+
+         cancelDraw: function cancelDraw() {
+            drawService.cancelDrawRectangle();
+         },
+
+         checkSize: function checkSize(clip) {
+            return _checkSize(clip);
+         }
+      };
+
+      function send(message, type, duration) {
+         flashService.remove(notify);
+
+         if (message) {
+            if (type === "error") {
+               messageService.error(message);
+            } else {
+               notify = flashService.add(message, duration, true);
+            }
+         }
+         /*
+           if (notify) {
+            notify.notify({
+               text: message,
+               type: type,
+               duration: duration
+            });
+         }
+         */
+      }
+
+      function _checkSize(clip) {
+         var deferred = $q.defer();
+         var result = drawn(clip);
+         if (result && result.code) {
+            switch (result.code) {
+               case "oversize":
+                  $timeout(function () {
+                     send("", "clear");
+                     send("The selected area is too large to process. Please restrict to approximately " + "1.5 degrees square.", "error");
+                     deferred.resolve(result);
+                  });
+                  break;
+               case "undersize":
+                  $timeout(function () {
+                     send("", "clear");
+                     send("X Min and Y Min should be smaller than X Max and Y Max, respectively. " + "Please update the drawn area.", "error");
+                     deferred.resolve(result);
+                  });
+                  break;
+               default:
+                  return $q.when(result);
+            }
+         }
+         return deferred.promise;
+      }
+
+      function underSizeLimit(clip) {
+         var size = (clip.xMax - clip.xMin) * (clip.yMax - clip.yMin);
+         return size < 0.00000000001 || clip.xMax < clip.xMin;
+      }
+
+      function overSizeLimit(clip) {
+         // Shouldn't need abs but it doesn't hurt.
+         var size = Math.abs((clip.xMax - clip.xMin) * (clip.yMax - clip.yMin));
+         return size > 2.25;
+      }
+
+      function forceNumbers(clip) {
+         clip.xMax = clip.xMax === null ? null : +clip.xMax;
+         clip.xMin = clip.xMin === null ? null : +clip.xMin;
+         clip.yMax = clip.yMax === null ? null : +clip.yMax;
+         clip.yMin = clip.yMin === null ? null : +clip.yMin;
+      }
+
+      function drawn(clip) {
+         //geoprocessService.removeClip();
+         forceNumbers(clip);
+
+         if (overSizeLimit(clip)) {
+            return { code: "oversize" };
+         }
+
+         if (underSizeLimit(clip)) {
+            return { code: "undersize" };
+         }
+
+         if (clip.xMax === null) {
+            return { code: "incomplete" };
+         }
+
+         if (validClip(clip)) {
+            return { code: "success" };
+         }
+         return { code: "invalid" };
+      }
+
+      // The input validator takes care of order and min/max constraints. We just check valid existance.
+      function validClip(clip) {
+         return clip && angular.isNumber(clip.xMax) && angular.isNumber(clip.xMin) && angular.isNumber(clip.yMax) && angular.isNumber(clip.yMin) && !overSizeLimit(clip) && !underSizeLimit(clip);
+      }
+
+      function getList(clip) {
+         configService.getConfig("processing").then(function (conf) {
+            var url = conf.intersectsUrl;
+            if (url) {
+               // Order matches the $watch signature so be careful
+               var urlWithParms = url.replace("{maxx}", clip.xMax).replace("{minx}", clip.xMin).replace("{maxy}", clip.yMax).replace("{miny}", clip.yMin);
+
+               send("Checking there is data in your selected area...", "wait", 180000);
+               $http.get(urlWithParms).then(function (response) {
+                  if (response.data && response.data.available_data) {
+                     var hasData = false;
+                     send("", "clear");
+                     if (response.data.available_data) {
+                        response.data.available_data.forEach(function (group) {
+                           if (group.downloadables) {
+                              decorateDownloadables(group.downloadables);
+                              hasData = true;
+                           }
+                        });
+                     }
+                     if (!hasData) {
+                        send("There is no data held in your selected area. Please try another area.", null, 4000);
+                     }
+
+                     $rootScope.$broadcast('site.selection', response.data);
+                  }
+               }, function (err) {
+                  // If it falls over we don't want to crash.
+                  send("The service that provides the list of datasets is currently unavailable. " + "Please try again later.", "error");
+               });
+            }
+         });
+
+         function decorateDownloadables(downloadables) {
+            Object.keys(downloadables).forEach(function (groupname) {
+               var group = downloadables[groupname];
+               Object.keys(group).forEach(function (listName) {
+                  var items = group[listName];
+
+                  items.forEach(function (item) {
+                     return decorateItem(item);
+                  });
+               });
+            });
+         }
+
+         function decorateItem(item) {
+            item.fileSize = fileSize(item.file_size);
+            if (item.product) {
+               //  "bbox" : "113,-44,154,-10"
+               var arr = item.bbox.split(",").map(function (num) {
+                  return +num;
+               });
+               item.bbox = [Math.max(arr[0], clip.xMin), Math.max(arr[1], clip.yMin), Math.min(arr[2], clip.xMax), Math.min(arr[3], clip.yMax)].join(",");
+            }
+         }
+      }
    }]);
 }
 "use strict";
