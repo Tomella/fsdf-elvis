abe85a56c434d482cdbbe37cb67545efae2c0367
Larry Hill <tomella1@gmail.com>
2020-05-27 23:43:23 +0930

    Allow proxying for developers

--- test/config.js
+++ test/config.js
@@ -1,5 +1,6 @@
 var config = {
     fmeToken: {
+       // isDev: true,
        tokenUrl: "http://elevation.fsdf.org.au/token",
        region: "ap-southeast-2",
        secretName: "fsdf/apps/fmecloud_token"
