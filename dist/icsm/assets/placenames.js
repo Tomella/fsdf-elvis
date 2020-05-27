611192a8e99bdfdef0ea9d2252b552e435d9a9ca
Larry Hill <tomella1@gmail.com>
2020-05-28 00:07:16 +0930

    Build the client with changes for token

--- dist/icsm/assets/placenames.js
+++ dist/icsm/assets/placenames.js
@@ -42,49 +42,20 @@ under the License.
 }
 "use strict";
 
-{
-  angular.module("placenames.summary", []).directive("placenamesSummary", ['$document', "$rootScope", "mapService", function ($document, $rootScope, mapService) {
-    return {
-      restrict: "AE",
-      templateUrl: "placenames/summary/summary.html",
-      link: function link(scope) {
-        $rootScope.$on("search.button.fired", function (event, item) {
-          console.log("item", item);
-          scope.remove();
-          scope.item = item;
-          scope.latLng = item.location.split(" ").map(function (num) {
-            return +num;
-          }).reverse();
-          mapService.getMap().then(function (map) {
-            scope.marker = L.marker(scope.latLng, {
-              icon: L.icon({
-                iconUrl: 'icsm/resources/img/marker-icon-red.png',
-                iconSize: [25, 41],
-                iconAnchor: [13, 41]
-              })
-            }).addTo(map);
-          });
-        });
+function getBounds(bounds, restrictTo) {
+  var fq;
 
-        scope.remove = function () {
-          if (scope.marker) scope.marker.remove();
-          scope.item = scope.marker = null;
-        };
+  if (restrictTo) {
+    var left = Math.max(bounds.getWest(), -180, restrictTo.getWest());
+    var right = Math.min(bounds.getEast(), 180, restrictTo.getEast());
+    var top = Math.min(bounds.getNorth(), 90, restrictTo.getNorth());
+    var bottom = Math.max(bounds.getSouth(), -90, restrictTo.getSouth());
+    fq = "location:[" + (bottom > top ? top : bottom) + "," + (left > right ? right : left) + " TO " + top + "," + right + "]";
+  } else {
+    fq = "location:[" + Math.max(bounds.getSouth(), -90) + "," + Math.max(bounds.getWest(), -180) + " TO " + Math.min(bounds.getNorth(), 90) + "," + Math.min(bounds.getEast(), 180) + "]";
+  }
 
-        scope.close = function () {
-          scope.remove();
-        };
-
-        $document.on('keydown', function keyupHandler(keyEvent) {
-          if (keyEvent.which === 27) {
-            scope.$apply(function () {
-              scope.remove();
-            });
-          }
-        });
-      }
-    };
-  }]);
+  return fq;
 }
 "use strict";
 
@@ -320,21 +291,50 @@ function SearchService($http, $rootScope, $timeout, placenamesConfigService, map
 }
 "use strict";
 
-function getBounds(bounds, restrictTo) {
-  var fq;
+{
+  angular.module("placenames.summary", []).directive("placenamesSummary", ['$document', "$rootScope", "mapService", function ($document, $rootScope, mapService) {
+    return {
+      restrict: "AE",
+      templateUrl: "placenames/summary/summary.html",
+      link: function link(scope) {
+        $rootScope.$on("search.button.fired", function (event, item) {
+          console.log("item", item);
+          scope.remove();
+          scope.item = item;
+          scope.latLng = item.location.split(" ").map(function (num) {
+            return +num;
+          }).reverse();
+          mapService.getMap().then(function (map) {
+            scope.marker = L.marker(scope.latLng, {
+              icon: L.icon({
+                iconUrl: 'icsm/resources/img/marker-icon-red.png',
+                iconSize: [25, 41],
+                iconAnchor: [13, 41]
+              })
+            }).addTo(map);
+          });
+        });
 
-  if (restrictTo) {
-    var left = Math.max(bounds.getWest(), -180, restrictTo.getWest());
-    var right = Math.min(bounds.getEast(), 180, restrictTo.getEast());
-    var top = Math.min(bounds.getNorth(), 90, restrictTo.getNorth());
-    var bottom = Math.max(bounds.getSouth(), -90, restrictTo.getSouth());
-    fq = "location:[" + (bottom > top ? top : bottom) + "," + (left > right ? right : left) + " TO " + top + "," + right + "]";
-  } else {
-    fq = "location:[" + Math.max(bounds.getSouth(), -90) + "," + Math.max(bounds.getWest(), -180) + " TO " + Math.min(bounds.getNorth(), 90) + "," + Math.min(bounds.getEast(), 180) + "]";
-  }
+        scope.remove = function () {
+          if (scope.marker) scope.marker.remove();
+          scope.item = scope.marker = null;
+        };
 
-  return fq;
+        scope.close = function () {
+          scope.remove();
+        };
+
+        $document.on('keydown', function keyupHandler(keyEvent) {
+          if (keyEvent.which === 27) {
+            scope.$apply(function () {
+              scope.remove();
+            });
+          }
+        });
+      }
+    };
+  }]);
 }
-angular.module('placenames.templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('placenames/summary/summary.html','<div class="placenames" ng-show="item">\r\n   <button class="undecorated placenames-unstick" ng-click="close()" style="float:right">X</button>\r\n   <div style="float:left">\r\n      <div class="container-fluid">\r\n         <div class="row">\r\n            <div class="col-md-12 pn-header placenames-title">\r\n               {{item.name}}\r\n            </div>\r\n         </div>\r\n      </div>\r\n      <div class="container-fluid">\r\n         <div class="row">\r\n            <div class="col-md-4" title="An authority can be a state department or other statutory authority">Authority</div>\r\n            <div class="col-md-8">{{item.authority}}</div>\r\n         </div>\r\n         <div class="row">\r\n            <div class="col-md-4" title="Features belong to a category and categories belong to a group">Feature Type</div>\r\n            <div class="col-md-8">{{item.feature}}</div>\r\n         </div>\r\n         <div class="row" title="Features belong to a category and categories belong to a group">\r\n            <div class="col-md-4">Category</div>\r\n            <div class="col-md-8">{{item.category}}</div>\r\n         </div>\r\n         <div class="row" title="Features belong to a category and categories belong to a group">\r\n            <div class="col-md-4">Group</div>\r\n            <div class="col-md-8">{{item.group}}</div>\r\n         </div>\r\n         <div class="row">\r\n            <div class="col-md-4">Lat / Lng</div>\r\n            <div class="col-md-8">\r\n               <span class="pn-numeric">\r\n                  {{latLng[0]}}&deg; / {{latLng[1]}}&deg;\r\n               </span>\r\n            </div>\r\n         </div>\r\n      </div>\r\n   </div>\r\n</div>');
-$templateCache.put('placenames/search/quicksearch.html','<div class="search-text" style="color:black; width: 26em" title="Start typing in the filter field. Up to twenty matches will be shown as you type with those nearest your map center at the top of the list. The results are restricted to your map\'s field of view so zooming the map in or out will change the number of results.">\r\n   <div class="input-group input-group-sm" style="width:100%">\r\n      <input class="hide"></input>\r\n      <input type="text" ng-model="state.filter" placeholder="Match by feature name..." placenames-on-enter="search($item, $model, $label)"\r\n         ng-model-options="{ debounce: 300}" typeahead-on-select="search($item, $model, $label)" typeahead-focus-first="false"\r\n         typeahead-template-url="placenames/search/typeahead.html" class="form-control" typeahead-min-length="1"\r\n         uib-typeahead="doc as doc.name for doc in loadDocs(state.filter)" typeahead-loading="loadingLocations" typeahead-no-results="noResults"\r\n         placenames-clear>\r\n   </div>\r\n</div>');
-$templateCache.put('placenames/search/typeahead.html','<a placenames-options ng-mouseenter="enter()" ng-mouseleave="leave()"  tooltip-append-to-body="true"\r\n               tooltip-placement="bottom" uib-tooltip-html="match.model | placenamesTooltip">\r\n   <span ng-bind-html="match.model.name | uibTypeaheadHighlight:query"></span>\r\n   (<span ng-bind-html="match.model.authority + \' - \' + match.model.feature"></span>)\r\n</a>');}]);
+angular.module('placenames.templates', []).run(['$templateCache', function($templateCache) {$templateCache.put('placenames/search/quicksearch.html','<div class="search-text" style="color:black; width: 26em" title="Start typing in the filter field. Up to twenty matches will be shown as you type with those nearest your map center at the top of the list. The results are restricted to your map\'s field of view so zooming the map in or out will change the number of results.">\r\n   <div class="input-group input-group-sm" style="width:100%">\r\n      <input class="hide"></input>\r\n      <input type="text" ng-model="state.filter" placeholder="Match by feature name..." placenames-on-enter="search($item, $model, $label)"\r\n         ng-model-options="{ debounce: 300}" typeahead-on-select="search($item, $model, $label)" typeahead-focus-first="false"\r\n         typeahead-template-url="placenames/search/typeahead.html" class="form-control" typeahead-min-length="1"\r\n         uib-typeahead="doc as doc.name for doc in loadDocs(state.filter)" typeahead-loading="loadingLocations" typeahead-no-results="noResults"\r\n         placenames-clear>\r\n   </div>\r\n</div>');
+$templateCache.put('placenames/search/typeahead.html','<a placenames-options ng-mouseenter="enter()" ng-mouseleave="leave()"  tooltip-append-to-body="true"\r\n               tooltip-placement="bottom" uib-tooltip-html="match.model | placenamesTooltip">\r\n   <span ng-bind-html="match.model.name | uibTypeaheadHighlight:query"></span>\r\n   (<span ng-bind-html="match.model.authority + \' - \' + match.model.feature"></span>)\r\n</a>');
+$templateCache.put('placenames/summary/summary.html','<div class="placenames" ng-show="item">\r\n   <button class="undecorated placenames-unstick" ng-click="close()" style="float:right">X</button>\r\n   <div style="float:left">\r\n      <div class="container-fluid">\r\n         <div class="row">\r\n            <div class="col-md-12 pn-header placenames-title">\r\n               {{item.name}}\r\n            </div>\r\n         </div>\r\n      </div>\r\n      <div class="container-fluid">\r\n         <div class="row">\r\n            <div class="col-md-4" title="An authority can be a state department or other statutory authority">Authority</div>\r\n            <div class="col-md-8">{{item.authority}}</div>\r\n         </div>\r\n         <div class="row">\r\n            <div class="col-md-4" title="Features belong to a category and categories belong to a group">Feature Type</div>\r\n            <div class="col-md-8">{{item.feature}}</div>\r\n         </div>\r\n         <div class="row" title="Features belong to a category and categories belong to a group">\r\n            <div class="col-md-4">Category</div>\r\n            <div class="col-md-8">{{item.category}}</div>\r\n         </div>\r\n         <div class="row" title="Features belong to a category and categories belong to a group">\r\n            <div class="col-md-4">Group</div>\r\n            <div class="col-md-8">{{item.group}}</div>\r\n         </div>\r\n         <div class="row">\r\n            <div class="col-md-4">Lat / Lng</div>\r\n            <div class="col-md-8">\r\n               <span class="pn-numeric">\r\n                  {{latLng[0]}}&deg; / {{latLng[1]}}&deg;\r\n               </span>\r\n            </div>\r\n         </div>\r\n      </div>\r\n   </div>\r\n</div>');}]);
