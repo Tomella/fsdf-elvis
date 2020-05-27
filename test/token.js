e851007f0601933890828239cc07a4ad3a88501c
Larry Hill <tomella1@gmail.com>
2020-05-27 22:31:41 +0930

    Prime the read of the secret

--- test/token.js
+++ test/token.js
@@ -4,7 +4,8 @@ let Token = require("../lib/token");
 (async function() {
 
     let token = new Token(config.fmeToken);
-    let value = await token.value;
-    console.log(value);
+    token.value.then(val => {
+        console.log(value);
+    });
 
 })();
