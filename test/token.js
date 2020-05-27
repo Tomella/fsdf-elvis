200f533d5744b406a89d323b92651bf2ac47be02
Larry Hill <tomella1@gmail.com>
2020-05-27 22:54:27 +0930

    Unfortunately have to commit to test on EC2 box

--- test/token.js
+++ test/token.js
@@ -4,7 +4,8 @@ let Token = require("../lib/token");
 (async function() {
 
     let token = new Token(config.fmeToken);
-    let value = await token.getToken();
-    console.log(value);
+    token.getToken().then(tok => {
+        console.log(tok);
+    });
 
 })();
